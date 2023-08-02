467204d7df04b51e467f778fc514d7486d3d38e9
HarshiniPR <harshiprana@gmail.com>
2023-08-02 22:42:24 +0530

    resources added

---
+++ frontend/Interview/interview.js
@@ -0,0 +1,31 @@
+const closeButton = document.querySelector("[data-close-button]");
+const ytElements = document.querySelectorAll(".yt");
+// Get the modal container element
+const modalContainer = document.querySelector(".yt-modal");
+
+//Function to open modal
+function openModal(videoId) {
+  const iframe = modalContainer.querySelector("iframe");
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+  modalContainer.style.display = "block"; // Show the modal
+}
+ytElements.forEach((ytElement) => {
+    ytElement.addEventListener('click', (event) => {
+      const imgElement = event.target.closest('img');
+      if (imgElement) {
+        const videoId = imgElement.getAttribute('data-video-id');
+        if (videoId) {
+          openModal(videoId);
+        }
+      }
+    });
+  });
+// Function to close the modal
+function closeModal() {
+  modalContainer.style.display = "none"; // Hide the modal
+  // If you want to stop the video when closing the modal, uncomment the line below
+  modalContainer.querySelector('iframe').src = '';
+}
+
+// Event listener for the close button
+closeButton.addEventListener("click", closeModal);
---
+++ frontend/Placement/internship.js
@@ -0,0 +1,20 @@
+
+
+
+var typed = new Typed("#texting-el", {
+    strings: ['🌟 Key Benefits of Our Program: 🌟'],
+   typeSpeed: 100,
+   backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+
+var typed = new Typed("#texting-el0", {
+    strings: ['Search your favourite mentor'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
