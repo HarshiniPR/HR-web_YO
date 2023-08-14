4ce79d34c2ae3d11abf8055cea38979f543e40fd
HarshiniPR <harshiprana@gmail.com>
2023-08-15 01:22:48 +0530

    template fetched in preview

--- frontend/Resume_build/createresume.js
+++ frontend/Resume_build/createresume.js
@@ -1,15 +1,27 @@
-const closeButton = document.querySelector("[data-close-button]");
-const pwButton = document.querySelector(".preview-btn");
 // Get the modal container element
 const modalContainer = document.querySelector(".modal");
 
 //Function to open modal
+const pwButton = document.querySelector(".preview-btn");
 function openModal() {
-  modalContainer.style.display = "block"; // Show the modal
+    modalContainer.style.display = "block"; // Show the modal
+    loadTemplateContent();
 }
-// Event listener to for preview button
 pwButton.addEventListener("click", openModal);
+// Load template content into modal
+function loadTemplateContent() {
+    const templateContentContainer = document.querySelector('#template-content');
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'template1.html', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        templateContentContainer.innerHTML = xhr.responseText; // Load content
+      }
+    };
+    xhr.send();
+  }
 // Function to close the modal
+const closeButton = document.querySelector("[data-close-button]");
 function closeModal() {
   modalContainer.style.display = "none"; // Hide the modal
 }
