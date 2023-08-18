0d04ab818a1b7eefb5d0981f59f3d30e5c7ebb1a
HarshiniPR <harshiprana@gmail.com>
2023-08-18 22:00:15 +0530

    total 5 templates added

--- frontend/Resume_build/createresume.js
+++ frontend/Resume_build/createresume.js
@@ -26,18 +26,18 @@ function closeModal() {
   modalContainer.style.display = "none"; // Hide the modal
 }
 closeButton.addEventListener("click", closeModal);
-
+//------------------------------------------------------------------------------------
 //determine the chosen template from the query parameter 
 //and load the corresponding template structure into the preview section.
-  const previewSection = document.getElementById('preview-section');
-  const queryParams = new URLSearchParams(window.location.search);
-  const chosenTemplate = queryParams.get('template');
+const previewSection = document.getElementById('preview-section');
+const queryParams = new URLSearchParams(window.location.search);
+const chosenTemplate = queryParams.get('template');
 
-  // Load the chosen template structure based on the query parameter
-  if (chosenTemplate === 'template1') {
-    fetch('template1.html') // Fetch the template1.html content
-      .then(response => response.text())
-      .then(templateContent => {
-        previewSection.innerHTML = templateContent; // Display the template content in the preview section
-      });
-  }
+// Load the chosen template structure based on the query parameter
+if (chosenTemplate) {
+  fetch(`${chosenTemplate}.html`) // Fetch the corresponding template content
+    .then(response => response.text())
+    .then(templateContent => {
+      previewSection.innerHTML = templateContent; // Display the template content in the preview section
+    });
+}
