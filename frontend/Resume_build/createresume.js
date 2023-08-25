81d11d9d15f57d90c419eee05a1daec993d5720b
HarshiniPR <harshiprana@gmail.com>
2023-08-25 12:12:51 +0530

    some changes in preview-section

--- frontend/Resume_build/createresume.js
+++ frontend/Resume_build/createresume.js
@@ -5,14 +5,14 @@ const modalContainer = document.querySelector(".modal");
 const pwButton = document.querySelector(".preview-btn");
 function openModal() {
   modalContainer.style.display = "block"; // Show the modal
-  loadTemplateContent();
+  loadTemplateContent(chosenTemplate);
 }
 pwButton.addEventListener("click", openModal);
 // Load template content into modal
-function loadTemplateContent() {
+function loadTemplateContent(templateName) {
   const templateContentContainer = document.querySelector("#template-content");
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "template1.html", true);
+  xhr.open("GET", `${templateName}.html`, true);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
       templateContentContainer.innerHTML = xhr.responseText; // Load content
@@ -26,6 +26,7 @@ function closeModal() {
   modalContainer.style.display = "none"; // Hide the modal
 }
 closeButton.addEventListener("click", closeModal);
+
 //------------------------------------------------------------------------------------
 //determine the chosen template from the query parameter
 //and load the corresponding template structure into the preview section.
@@ -38,74 +39,31 @@ if (chosenTemplate) {
   fetch(`${chosenTemplate}.html`) // Fetch the corresponding template content
     .then((response) => response.text())
     .then((templateContent) => {
-      // Create a wrapping div for the template content
-      const templateWrapper = document.createElement("div");
-      templateWrapper.classList.add("zoom-out-container"); // Add the zoom-out class to the wrapper
-      templateWrapper.innerHTML = templateContent;
-
-      // Append the wrapped template content to the preview section
-      previewSection.appendChild(templateWrapper);
+      previewSection.innerHTML = templateContent;
     });
 }
 
-//live preview starts
 
-// const aboutInputs = document.querySelector(".cv-form-about input");
-// aboutInputs.forEach((input) => {
-//   input.addEventListener("input", updatePreview);
-// });
 
-// function updatePreview() {
-//   const firstName = document.querySelector("firstname").value;
-//   const lastName = document.querySelector("lastname").value;
-//   const designation = document.querySelector("designation").value;
-//   const address = document.querySelector(".address").value;
-//   const email = document.querySelector(".email").value;
-//   const phoneNo = document.querySelector(".phoneno").value;
-//   const summary = document.querySelector(".summary").value;
 
-//   // Update the preview section in createresume.html
-//   previewSection.innerHTML = `
-//     <h2> ${firstName} ${lastName}</h2>
-//     <p>${designation}</p>
-//     <p>${address}</p>
-//     <p>${email}</p>
-//     <p>${phoneNo}</p>
-//     <p>${summary}</p>`;
 
-//     const aboutData = {
-//       firstName,
-//       lastName,
-//       designation,
-//       address,
-//       email,
-//       phoneNo,
-//       summary,
-//     };
-//     localStorage.setItem("resumeData", JSON.stringify(aboutData));
-
-// }
 // Add an event listener to each input element in the form
-
+//======================================================================================================
 // Update the preview section based on the input values
 window.onload = function () {
   function updatePreview() {
+    //Select element from createresume.html
+    //ABOUT Section
     const firstName = document.querySelector(".firstname").value;
     const lastName = document.querySelector(".lastname").value;
     const designation = document.querySelector(".designation").value;
-    console.log("Hello");
-    console.log(designation);
     const address = document.querySelector(".address").value;
     const email = document.querySelector(".email").value;
     const phoneNo = document.querySelector(".phoneno").value;
     const summary = document.querySelector(".summary").value;
 
-    // Update the corresponding elements in the template preview
-    const profileName = document.querySelector(".profileText h2");
 
-    const profileDesignation = profileName.querySelector("#profDes");
-    console.log("HI");
-    console.log(profileDesignation);
+    const profileName = document.querySelector(".profileText h2");
     const contactPhone = document.querySelector(".text.phone");
     const contactEmail = document.querySelector(".text.email");
     const contactAddress = document.querySelector(".text.location");
@@ -113,18 +71,76 @@ window.onload = function () {
 
     // Update the elements in the preview
     profileName.innerHTML = `${firstName} ${lastName} <br/> <span id="profDes">${designation}</span>`;
-    console.log("now "+profileName);
-
-    profileDesignation.textContent = designation;
-    console.log("now2 "+profileDesignation);
-
     contactPhone.textContent = phoneNo;
     contactEmail.textContent = email;
     contactAddress.textContent = address;
     aboutSummary.textContent = summary;
 
+    //ACHIEVEMENTS Section
+    const achievement = document.querySelector(".achieve_title").value;
+    const achieveDesc = document.querySelector(".achieve_description").value;
+    //EXPERIENCE Section
+    const expName = document.querySelector(".exp_title").value;
+    const expCompany = document.querySelector(".exp_company").value;
+    const expSY = document.querySelector(".exp_sy").value;
+    const expEY = document.querySelector(".exp_ey").value;
+    const expDesc = document.querySelector(".exp_description").value;
+
+    const experienceTitle = document.querySelector(".role_title h4");
+    const company = document.querySelector(".comp_name");
+    const experiencePeriod = document.querySelector(".exp_period");
+    const experienceDesc = document.querySelector(".desc_company");
+
+    experiencePeriod.innerHTML = `<div class="year_company">
+    <h5>${expSY} - ${expEY}</h5>
+  </div>`;
+    company.textContent = expCompany;
+    experienceDesc.textContent = expDesc;
+    experienceTitle.textContent = expName;
+
+    //EDUCATION Section
+    const schoolClg = document.querySelector(".edu_school").value;
+    const degree = document.querySelector(".edu_degree").value;
+    const eduSY = document.querySelector(".edu_sy").value;
+    const eduEY = document.querySelector(".edu_ey").value;
+    const percentage = document.querySelector(".edu_percent").value;
+
+    const educationPeriod = document.querySelector(".edu_period");
+    const eduDegree = document.querySelector(".degree");
+    const schoolName = document.querySelector(".uni");
+
+
+    educationPeriod.innerHTML = ` ${eduSY} - ${eduEY}`;
+    eduDegree.textContent = degree;
+    schoolName.textContent = schoolClg;
+    //PROJECTS Section
+    const projectTitle = document.querySelector(".proj_title").value;
+    const projectLink = document.querySelector(".proj_link").value;
+    const projectDesc = document.querySelector(".proj_description").value;
+    //SKILLS Section
+    const skillinput = document.querySelector(".skill").value;
+    const interestinput = document.querySelector(".interestInp").value;
+
+    const skills = document.querySelector('.skills h4');
+    const interest = document.querySelector('.interest li');
+
+    skills.textContent = skillinput;
+    //====================================================================================================
+    // Update the corresponding elements in the template preview
+
+    //ACHIEVEMENTS Section
+    // const achieveTitle = document.querySelector(".achieve_title");
+    // const achieveDescription = document.querySelector(".achieve_description");
+    //EXPERIENCE Section
+    
+    //EDUCATION Section
+    //PROJECTS Section
+    //SKILLS Section
+    
+
+   
     // Store the updated data in localStorage
-    const aboutData = {
+    const Data = {
       firstName,
       lastName,
       designation,
@@ -132,14 +148,17 @@ window.onload = function () {
       email,
       phoneNo,
       summary,
+      expSY,
+      expEY,
+      expCompany,
+      expDesc,
+      expName,
     };
-    localStorage.setItem("resumeData", JSON.stringify(aboutData));
-    console.log(aboutData)
+    localStorage.setItem("resumeData", JSON.stringify(Data));
   }
-  
-  const inputElements = document.querySelectorAll(".cv-form-about input");
+
+  const inputElements = document.querySelectorAll(".content_box input");
   inputElements.forEach((input) => {
     input.addEventListener("input", updatePreview);
   });
-  
 };
