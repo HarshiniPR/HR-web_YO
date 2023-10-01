3a22399cd6be64b548b9fbb45779148fa2c620ab
ankitmishra8652 <ankitmishra8652@gmail.com>
2023-10-01 14:06:10 +0530

    employer login not solved here

--- frontend/Resume_build/createresume.js
+++ frontend/Resume_build/createresume.js
@@ -11,21 +11,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 2. Function to update the preview
   function updatePreview() {
-
     function updatePreview() {
       // Get the education sections from the modal
       const educationSections = document.querySelectorAll(".education");
-    
+
       // Loop through the education sections and update the preview
       for (const educationSection of educationSections) {
-        const schoolInput = educationSection.querySelector("input[name='edu_school']");
-        const degreeInput = educationSection.querySelector("input[name='edu_degree']");
-        const periodInput = educationSection.querySelector("input[name='edu_period']");
-    
+        const schoolInput = educationSection.querySelector(
+          "input[name='edu_school']"
+        );
+        const degreeInput = educationSection.querySelector(
+          "input[name='edu_degree']"
+        );
+        const periodInput = educationSection.querySelector(
+          "input[name='edu_period']"
+        );
+
         // Update the preview with the input values
-        educationSection.querySelector(".edu_school").textContent = schoolInput.value;
-        educationSection.querySelector(".edu_degree").textContent = degreeInput.value;
-        educationSection.querySelector(".edu_period").textContent = periodInput.value;
+        educationSection.querySelector(".edu_school").textContent =
+          schoolInput.value;
+        educationSection.querySelector(".edu_degree").textContent =
+          degreeInput.value;
+        educationSection.querySelector(".edu_period").textContent =
+          periodInput.value;
       }
     }
     // Store the updated data in localStorage
@@ -54,38 +62,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const addEducationButton = document.querySelector("#addEducation");
 
-  // Add an event listener to the "+" button
-  addEducationButton.addEventListener("click", addEducationEntry);
+// Add an event listener to the "+" button
+addEducationButton.addEventListener("click", addEducationEntry);
 
-  function addEducationEntry() {
-    // Create new elements to represent the education entry
-    const newLi = document.createElement("li");
-    const period = document.createElement("h5");
-    const degree = document.createElement("h4");
-    const school = document.createElement("h4");
+function addEducationEntry() {
+  // Create new elements to represent the education entry
+  const newLi = document.createElement("li");
+  const period = document.createElement("h5");
+  const degree = document.createElement("h4");
+  const school = document.createElement("h4");
 
-    // Get the input values for the new education entry
-    const eduSY = document.querySelector(".edu_sy").value;
-    const eduEY = document.querySelector(".edu_ey").value;
-    const degreeValue = document.querySelector(".edu_degree").value;
-    const schoolValue = document.querySelector(".edu_school").value;
+  // Get the input values for the new education entry
+  const eduSY = document.querySelector(".edu_sy").value;
+  const eduEY = document.querySelector(".edu_ey").value;
+  const degreeValue = document.querySelector(".edu_degree").value;
+  const schoolValue = document.querySelector(".edu_school").value;
 
-    // Set the content for the new elements
-    period.textContent = `${eduSY}-${eduEY}`;
-    degree.textContent = degreeValue;
-    school.textContent = schoolValue;
+  // Set the content for the new elements
+  period.textContent = `${eduSY}-${eduEY}`;
+  degree.textContent = degreeValue;
+  school.textContent = schoolValue;
 
-    // Append the new elements to the list item (li)
-    newLi.appendChild(period);
-    newLi.appendChild(degree);
-    newLi.appendChild(school);
+  // Append the new elements to the list item (li)
+  newLi.appendChild(period);
+  newLi.appendChild(degree);
+  newLi.appendChild(school);
 
-    // Find the list of education entries in template1
-    const educationList = document.querySelector(".education ul");
+  // Find the list of education entries in template1
+  const educationList = document.querySelector(".education ul");
 
-    // Append the new education entry (li) to the education list
-    educationList.appendChild(newLi);
-  }
+  // Append the new education entry (li) to the education list
+  educationList.appendChild(newLi);
+}
 // 3. Function to open the modal
 const modalContainer = document.querySelector(".modal");
 function openModal() {
@@ -109,7 +117,6 @@ function openModal() {
         const summary = document.querySelector(".summary").value;
         const imageInput = document.querySelector(".image");
 
-
         const displayImage = document.querySelector("#displayImage");
         const profileName = document.querySelector(".profileText h2");
         const contactPhone = document.querySelector(".text.phone");
@@ -118,7 +125,7 @@ function openModal() {
         const inputWeb = document.querySelector(".myweb");
         const contactAddress = document.querySelector(".text.location");
         const aboutSummary = document.querySelector(".about p");
-      
+
         profileName.innerHTML = `${firstName} ${lastName} <br/> <span id="profDes">${designation}</span>`;
         contactPhone.innerHTML = `<span class="icon"><i class="fa-solid fa-phone"></i></span>
         <span class="text ">${phoneNo}</span>`;
@@ -182,10 +189,12 @@ function openModal() {
 
           // Update the preview of the newly added education section
         });
-        
+
         //ACHIEVEMENTS Section
         const achieveInput = document.querySelector(".achieve_title").value;
-        const achieveDescInput = document.querySelector(".achieve_description").value;
+        const achieveDescInput = document.querySelector(
+          ".achieve_description"
+        ).value;
         const achievement = document.querySelector(".achievement-item");
 
         achievement.innerHTML = `<h4>${achieveInput} </h4> <div class="achievedesc"> | ${achieveDescInput}</div>`;
--- frontend/auth.js
+++ frontend/auth.js
@@ -1,116 +1,120 @@
-document.getElementById('registrationFormSubmit').addEventListener('click', function (event) {
+document
+  .getElementById("registrationFormSubmit")
+  .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
 
     const formData = {
-        firstName,
-        lastName,
-        email,
-        password,
-        confirmPassword,
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
     };
-    console.log(formData)
+    console.log(formData);
 
-    fetch('http://localhost:4002/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+    fetch("http://localhost:4002/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
-        .then(response => response.json())
-        .then((data) => {
-            if (data.success) {
-                console.log(data)
-                console.log("Authenticated")
-                window.location.href = 'index.html';
-            } else {
-                if (data.message)
-                    window.alert(data.message)
-                if (data.errors)
-                    window.alert(data.errors)
-                console.log('Registration failed:', data);
-            }
-        })
-        .catch(error => {
-            console.error('ye Error h:', error);
-        });
-});
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log(data);
+          console.log("Authenticated");
+          window.location.href = "index.html";
+        } else {
+          if (data.message) window.alert(data.message);
+          if (data.errors) window.alert(data.errors);
+          console.log("Registration failed:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("ye Error h:", error);
+      });
+  });
 
-document.getElementById('button').addEventListener('click', async function (event) {
+document
+  .getElementById("button")
+  .addEventListener("click", async function (event) {
     event.preventDefault();
 
-    const email = document.getElementById('username').value;
-    const password = document.getElementById('loginpassword').value;
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("loginpassword").value;
 
     const formData = {
-        email,
-        password
+      email,
+      password,
     };
-    console.log(formData)
+    console.log(formData);
 
-    fetch('http://localhost:4002/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify(formData),
+    fetch("http://localhost:4002/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
     })
-        .then(response => response.json())
-        .then((data) => {
-            if (data.success) {
-                console.log(data)
-                console.log("Authenticated")
-                window.location.href = 'index.html';
-            } else {
-                if (data.message === "User not found") {
-                    window.alert("This email is not found. Please register first")
-                }
-                else{
-                    window.alert(data.message)
-                }
-                console.log('login failed:', data);
-            }
-        })
-        .catch(error => {
-            console.error('ye Error h:', error);
-        });
-})
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log(data);
+          console.log("Authenticated");
+          window.location.href = "index.html";
+        } else {
+          if (data.message === "User not found") {
+            window.alert("This email is not found. Please register first");
+          } else {
+            window.alert(data.message);
+          }
+          console.log("login failed:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("ye Error h:", error);
+      });
+  });
 
-document.getElementById('forgotpassword').addEventListener('click', async function (event){
+document
+  .getElementById("forgotpassword")
+  .addEventListener("click", async function (event) {
     event.preventDefault();
-    window.alert("your reset password link will be sent on the email entered here")
-    const email = window.prompt("Enter email")
-
+    window.alert(
+      "your reset password link will be sent on the email entered here"
+    );
+    const email = window.prompt("Enter email");
 
     const formData = {
-        email
+      email,
     };
 
-    fetch('http://localhost:4002/api/password/forgot', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+    fetch("http://localhost:4002/api/password/forgot", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
-        .then(response => response.json())
-        .then((data) => {
-            if (data.success) {
-                console.log(data)
-                console.log("Authenticated")
-                window.alert("mail sent")
-            } else {
-                console.log('failed:', data);
-            }
-        })
-        .catch(error => {
-            console.error('ye Error h:', error);
-        });
-})
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          console.log(data);
+          console.log("Authenticated");
+          window.alert("mail sent");
+        } else {
+          console.log("failed:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("ye Error h:", error);
+      });
+  });
