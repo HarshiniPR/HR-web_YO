f72886734e3267e5fe13b46cb0e416f2b23b74b7
Sreeja Ghosh <77427558+Sreejavastar@users.noreply.github.com>
2023-10-13 16:57:42 +0530

    Service-tools, image placeholders

--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,3 @@
-//login and register section starts here
-
-// const body = document.querySelector("body");
-const wrapper = document.querySelector(".wrapper");
-const wrapper2 = document.querySelector(".wrapper2");
-const loginForm = document.getElementById("loginForm");
-const cancelBtn = document.querySelector(".cancelButton");
-
-function toggleLoginForm() {
-  wrapper2.classList.toggle("show");
-  body.style.overflow = "hidden";
-  // Disable pointer events on all elements except the form
-  const elements = document.querySelectorAll("*");
-  elements.forEach((element) => {
-    if (!element.closest(".inner2")) {
-      element.classList.add("disable-pointer-events");
-    }
-  });
-}
-
-function toggleRegisterForm() {
-  wrapper.classList.toggle("show");
-  body.style.overflow = "hidden";
-  // Disable pointer events on all elements except the form
-  const elements = document.querySelectorAll("*");
-  elements.forEach((element) => {
-    if (!element.closest(".inner")) {
-      element.classList.add("disable-pointer-events");
-    }
-  });
-}
-
 const section4 = document.querySelector(".section-4");
 const section4Left1 = document.querySelector(".section-4-left-1");
 const section4Left2 = document.querySelector(".section-4-left-2");
@@ -43,6 +11,15 @@ const section4RightImg2 = "./images/internship page.gif";
 const section4RightImg3 = "./images/resume building page.gif";
 const section4RightImg4 = "./images/job listings page.gif";
 const section4RightImg5 = "./images/Linkedin Branding.gif";
+
+//animated Text
+ var typed = new Typed("#textingfirst", {
+   strings: [" Success"],
+   typeSpeed: 100,
+   backSpeed: 100,
+   backDelay: 1000,
+   loop: true,
+ });
 
 // Define scroll ranges for each section
 const sectionRanges = [
@@ -91,68 +68,96 @@ function updateContentAndImage(scrollPosition) {
   ][imgIndex];
 }
 
-// Call updateContentAndImage on initial load
-updateContentAndImage(2);
-
 // Add scroll event listener
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
   const section4OffsetTop = section4.offsetTop;
+
   // Update content and image based on scroll position
   updateContentAndImage(scrollPosition - section4OffsetTop);
 });
 
-// // SLIDER JS
-// const slider2 = document.querySelector(".slider2");
-// const images = document.querySelectorAll(".slider2 img");
-// const prevBtn = document.getElementById("prevBtn");
-// const nextBtn = document.getElementById("nextBtn");
-// let currentImageIndex = 0;
-// const imageWidth = images[0].clientWidth;
-// let isTransitioning = false;
+// Call updateContentAndImage on initial load
+updateContentAndImage(2);
 
-// // Function to show the current image
-// function showImage(index) {
-//   isTransitioning = true;
-//   slider2.style.transform = `translateX(-${index * imageWidth}px)`;
-// }
+//login and register section starts here
 
-// // Event listener for the previous button
-// prevBtn.addEventListener("click", function () {
-//   if (!isTransitioning) {
-//     currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-//     showImage(currentImageIndex);
-//   }
-// });
+const body = document.querySelector(".blur");
+const wrapper = document.querySelector(".wrapper");
+const wrapper2 = document.querySelector(".wrapper2");
+const loginForm = document.getElementById("loginForm");
+const cancelBtn = document.querySelector(".cancelButton");
 
-// // Event listener for the next button
-// nextBtn.addEventListener("click", function () {
-//   if (!isTransitioning) {
-//     currentImageIndex = (currentImageIndex + 1) % images.length;
-//     showImage(currentImageIndex);
-//   }
-// });
+function toggleLoginForm() {
+  body.classList.add("blur");
+  wrapper2.classList.toggle("show");
+  body.style.overflow = "hidden";
+  // Disable pointer events on all elements except the form
+  const elements = document.querySelectorAll("*");
+  elements.forEach((element) => {
+    if (!element.closest(".inner2")) {
+      element.classList.add("disable-pointer-events");
+    }
+  });
+}
 
-// // Add transitionend event listener to reset the isTransitioning flag
-// slider2.addEventListener("transitionend", function () {
-//   isTransitioning = false;
-// });
+function toggleRegisterForm() {
+  body.classList.add("blur");
+  wrapper.classList.toggle("show");
+  body.style.overflow = "hidden";
+  // Disable pointer events on all elements except the form
+  const elements = document.querySelectorAll("*");
+  elements.forEach((element) => {
+    if (!element.closest(".inner")) {
+      element.classList.add("disable-pointer-events");
+    }
+  });
+}
 
-// // Clone the first and last images for seamless transition
-// const firstImageClone = images[0].cloneNode(true);
-// const lastImageClone = images[images.length - 1].cloneNode(true);
-// slider2.appendChild(firstImageClone);
-// slider2.insertBefore(lastImageClone, images[0]);
+//login and register section ends here
 
-// // Adjust the slider width and initial position
-// slider2.style.width = `${imageWidth * (images.length + 2)}px`;
-// slider2.style.transform = `translateX(-${imageWidth}px)`;
+// SLIDER JS
+const slider2 = document.querySelector(".slider2");
+const images = document.querySelectorAll(".slider2 img");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+let currentImageIndex = 0;
+const imageWidth = images[0].clientWidth;
+let isTransitioning = false;
 
-// dynamic texting
-var typed = new Typed("#textingfirst", {
-  strings: ["Success"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
+// Function to show the current image
+function showImage(index) {
+  isTransitioning = true;
+  slider2.style.transform = `translateX(-${index * imageWidth}px)`;
+}
+
+// Event listener for the previous button
+prevBtn.addEventListener("click", function () {
+  if (!isTransitioning) {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+  }
 });
+
+// Event listener for the next button
+nextBtn.addEventListener("click", function () {
+  if (!isTransitioning) {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+  }
+});
+
+// Add transitionend event listener to reset the isTransitioning flag
+slider2.addEventListener("transitionend", function () {
+  isTransitioning = false;
+});
+
+// Clone the first and last images for seamless transition
+const firstImageClone = images[0].cloneNode(true);
+const lastImageClone = images[images.length - 1].cloneNode(true);
+slider2.appendChild(firstImageClone);
+slider2.insertBefore(lastImageClone, images[0]);
+
+// Adjust the slider width and initial position
+slider2.style.width = `${imageWidth * (images.length + 2)}px`;
+slider2.style.transform = `translateX(-${imageWidth}px)`;
